9872a4de9c3a3c37c2bcb77ed942047b455c6d2e
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-06-25 14:00:05 +0600

    fix: instructor login and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/parent/:path*", "/student/:path*"],
+  matcher: ["/parent/:path*", "/student/:path*", "/instructor/:path*"],
 };
