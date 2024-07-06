05999af106b8c0f9e6a10a74696eb129fb936197
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-07-06 15:50:10 +0600

    fix: deleted react-typewritter

--- src/components/pageComponents/HomepageComponents/Hero.tsx
+++ src/components/pageComponents/HomepageComponents/Hero.tsx
@@ -2,7 +2,7 @@
 import astroBoy from "../../../../public/chapter/astroBoy.json";
 import Lottie from "lottie-react";
 import Link from "next/link";
-import TypeWriterEffect from "react-typewriter-effect";
+// import TypeWriterEffect from "react-typewriter-effect";
 
 const Hero = () => {
   return (
@@ -10,26 +10,7 @@ const Hero = () => {
       <div className="flex flex-col md:flex-row items-center justify-between gap-8">
         <div className="text-center md:text-left md:w-1/2">
           <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-8">
-            <TypeWriterEffect
-              textStyle={{
-                fontWeight: 700,
-                fontSize: "inherit",
-              }}
-              startDelay={100}
-              cursorColor="#3F3D56"
-              multiText={[
-                "Learn to Code",
-                "Through the Power of Play",
-                "Discover the Magic of Coding",
-                "Unlock Your Creative Potential",
-                "Transform Ideas into Code",
-                "Master Programming with Fun",
-                "Build Your Future in Tech",
-              ]}
-              multiTextDelay={1000}
-              typeSpeed={100}
-              loop={true}
-            />
+            Learn to code through the power of play!
           </h1>
           <div className="space-y-4 max-w-sm mx-auto md:mx-0">
             <Link href="/login" passHref>
